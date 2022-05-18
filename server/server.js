6a0d3b8c773b08552a68d0b498913f880877e7f4
chrisyuen976 <chrisyuen976@gmail.com>
2022-05-18 21:23:41 +1000

    push heroku

--- server/server.js
+++ server/server.js
@@ -15,12 +15,13 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
 
 if (process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
+    app.use(express.static('../client/build'))
 };
 
 const path = require('path');
 app.get('*', (req,res)=>{
-    res.sendFile(path.resolve('../', __dirname, 'client','build','index.html'))
+    const index = path.join(__dirname, 'client', 'build', 'index.html')
+    res.sendFile(index)
 })
 
 
