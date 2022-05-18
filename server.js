f5bbe2767a4aa66c24565ae4dc54ff46ad5e558f
chrisyuen976 <chrisyuen976@gmail.com>
2022-05-18 18:57:39 +1000

    push heroku

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const app = express()
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
 
+app.use(express.static(__dirname + '/client'));
+
 
 const searchDatabase = async function(username){
     try{
